2b66a3421e519de5a57fba0468de3f1eadc1b090
lordbman <okelekelenobel@gmail.com>
2026-06-25 20:32:04 +0000

    topbar title color now changes on collapse

--- app/src/main/java/com/bsoft/compose/bmusic/ui/components/ImageBackgroundTopAppBar.kt
+++ app/src/main/java/com/bsoft/compose/bmusic/ui/components/ImageBackgroundTopAppBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -13,12 +14,21 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ImageBackgroundTopAppBar(modifier: Modifier = Modifier, title: String, @DrawableRes image: Int, scrollBehavior: TopAppBarScrollBehavior? = null, navigationIcon: @Composable (() -> Unit) = {}) {
+fun ImageBackgroundTopAppBar(modifier: Modifier = Modifier, title: String, @DrawableRes image: Int, scrollBehavior: TopAppBarScrollBehavior, navigationIcon: @Composable (() -> Unit) = {}) {
+    val collapsedFraction = scrollBehavior.state.collapsedFraction
+
+    // 3. Linearly interpolate between your expanded and collapsed colors
+    val titleColor = lerp(
+        start = Color.White,    // Color when fully EXPANDED
+        stop = MaterialTheme.colorScheme.onSurface,      // Color when fully COLLAPSED
+        fraction = collapsedFraction
+    )
     // The outer Box constraints the stack layer
     Box(modifier = modifier.fillMaxWidth()) {
         Image(
@@ -32,7 +42,10 @@ fun ImageBackgroundTopAppBar(modifier: Modifier = Modifier, title: String, @Draw
             title = { Text(title) },
             navigationIcon = navigationIcon,
             scrollBehavior = scrollBehavior,
-            colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent)
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent,
+                navigationIconContentColor = titleColor,
+                titleContentColor = titleColor,
+                actionIconContentColor = titleColor )
         )
     }
 }
